af28f0b2a7dcaa7c35e69a36f4a8456b946fceff
sslubik <szymonslubik@gmail.com>
2024-04-15 19:52:19 +0200

    Add mappings for vim-illuminate plugin

--- lua/plugins/vim-illuminate.lua
+++ lua/plugins/vim-illuminate.lua
@@ -1,3 +1,15 @@
 return {
 	"RRethy/vim-illuminate",
+
+    config = function()
+        local illuminate = require("illuminate")
+        illuminate.configure()
+        
+        vim.keymap.set(
+            "n", "]", function() illuminate.goto_next_reference(false) end
+        )
+        vim.keymap.set(
+            "n", "[", function() illuminate.goto_prev_reference(false) end
+        )
+    end,
 }
