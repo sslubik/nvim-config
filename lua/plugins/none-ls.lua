33e2e1c35c905b94c146a2725310e1665702c104
Szymon Ślubik <szymon.slubik@besidethepark.com>
2024-05-14 15:53:14 +0200

    Fixed typo

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -8,7 +8,7 @@ return {
                 null_ls.builtins.formatting.stylua,
                 null_ls.builtins.formatting.prettier,
                 null_ls.builtins.formatting.phpcbf,
-                null_ls.builtins.formatting.balck,
+                null_ls.builtins.formatting.black,
                 null_ls.builtins.formatting.isort,
             },
         })
